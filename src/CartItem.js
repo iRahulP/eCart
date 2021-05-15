4caaf3aee4a493efa0c02ce1008f068afbc79ce3
iRahulP <rahulpatil.m30@gmail.com>
2021-05-15 07:34:55 +0530

    updated img in component

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,6 +13,9 @@ class CartItem extends React.Component {
                     <div style={{ color: '#777' }}>Qty: 1</div>
                     <div className="cart-item-acions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://img-premium.flaticon.com/png/512/992/992651.png?token=exp=1621017251~hmac=e4f50b4edf771dd804128b77c9281b3d" />
+                        <img alt="decrease" className="action-icons" src="https://t4.ftcdn.net/jpg/03/16/36/03/240_F_316360373_uWcj5rZxsUbmoAogMfow8EZhUOn7FTM0.jpg" />
+                        <img alt="delete" className="action-icons" src="https://img-premium.flaticon.com/png/512/1345/1345874.png?token=exp=1621017310~hmac=2238a7c112b4af8d932321ecf4343af1" />
                     </div>
                 </div>
             </div >
