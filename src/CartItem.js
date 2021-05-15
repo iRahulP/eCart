38fb72254da799363f5f848986452ab885c3aebe
iRahulP <rahulpatil.m30@gmail.com>
2021-05-15 07:49:33 +0530

    updated cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+
+    //Constructor for CartItem
+    constructor() {
+        //super() calls constructor of Parent class i.e React.Component
+        super();
+        this.state = {
+            price: 999,
+            title: 'Z Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
     render() {
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 25 }}>X Phone</div>
-                    <div style={{ color: '#777' }}>Rs 99999</div>
-                    <div style={{ color: '#777' }}>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Price : {price}$</div>
+                    <div style={{ color: '#777' }}>Qty : {qty}</div>
                     <div className="cart-item-acions">
                         {/* Buttons */}
                         <img alt="increase" className="action-icons" src="https://img-premium.flaticon.com/png/512/992/992651.png?token=exp=1621017251~hmac=e4f50b4edf771dd804128b77c9281b3d" />
